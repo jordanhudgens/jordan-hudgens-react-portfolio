5f1a1ca2bf5d960c3495ef9236760b487e30c2ae
jordanhudgens <jordan@devcamp.com>
2018-11-29 10:57:34 -0700

    Created a portfolio container component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Jordan Hudgens Portfolio</h1>
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
       </div>
     );
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PortfolioContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Portfolio items go here updated...</h2>
+      </div>
+    );
+  }
+}
