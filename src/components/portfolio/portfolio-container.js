253e84941d088c252fecacd3c7006313107ba35c
jordanhudgens <jordan@devcamp.com>
2018-12-03 09:54:09 -0700

    B_07_introduction_to_state_in_react

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,21 +6,27 @@ export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
-    console.log("Portfolio container has rendered");
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      data: [
+        { title: "Quip" },
+        { title: "Eventbrite" },
+        { title: "Ministry Safe" },
+        { title: "SwingAway" }
+      ]
+    };
   }
 
   portfolioItems() {
-    const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"];
-
-    return data.map(item => {
-      return <PortfolioItem title={item} url={"google.com"} />;
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
     });
   }
 
   render() {
     return (
       <div>
-        <h2>Portfolio items go here updated...</h2>
+        <h2>{this.state.pageTitle}</h2>
 
         {this.portfolioItems()}
       </div>
