f29e000320de220a8e8011b9160ae6c9dd5739f9
jordanhudgens <jordan@devcamp.com>
2018-12-03 13:08:31 -0700

    B_10_introduction_react_conditionals

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,6 +8,7 @@ export default class PortfolioContainer extends Component {
 
     this.state = {
       pageTitle: "Welcome to my portfolio",
+      isLoading: false,
       data: [
         { title: "Quip", category: "eCommerce" },
         { title: "Eventbrite", category: "Scheduling" },
@@ -34,6 +35,10 @@ export default class PortfolioContainer extends Component {
   }
 
   render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
