2bd38cde5f26b730bfe98d5aabeb14c8688a7f9b
jordanhudgens <jordan@devcamp.com>
2018-12-04 12:01:24 -0700

    C_03_nav_link_active_styles

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,8 @@ import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
@@ -18,6 +20,8 @@ export default class App extends Component {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
             </Switch>
           </div>
         </Router>
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -9,14 +9,20 @@ export default class NavigationComponent extends Component {
   render() {
     return (
       <div>
-        <NavLink exact to="/">
+        <NavLink exact to="/" activeClassName="nav-link-active">
           Home
         </NavLink>
 
-        <NavLink to="/about-me">About</NavLink>
+        <NavLink to="/about-me" activeClassName="nav-link-active">
+          About
+        </NavLink>
+        <NavLink to="/contact" activeClassName="nav-link-active">
+          Contact
+        </NavLink>
+        <NavLink to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
 
-        <button>Contact</button>
-        <button>Blog</button>
         {false ? <button>Add Blog</button> : null}
       </div>
     );
