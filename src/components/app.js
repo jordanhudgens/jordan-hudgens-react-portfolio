8488241d2a9d156393c1c1ae8eadbb5fbf1f1889
jordanhudgens <jordan@devcamp.com>
2018-12-04 10:30:42 -0700

    C_01_basic_route_setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <NavigationContainer />
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Jordan Hudgens Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
