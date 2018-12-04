54974161584719a8c3918efbf3225678157bfd60
jordanhudgens <jordan@devcamp.com>
2018-12-04 10:49:51 -0700

    C_02_overview_of_react_router_nav_links

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
   constructor() {
@@ -8,8 +9,12 @@ export default class NavigationComponent extends Component {
   render() {
     return (
       <div>
-        <button>Home</button>
-        <button>About</button>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+
+        <NavLink to="/about-me">About</NavLink>
+
         <button>Contact</button>
         <button>Blog</button>
         {false ? <button>Add Blog</button> : null}
