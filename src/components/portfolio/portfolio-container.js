fc8df17410a6820de6fc41079a29aaf32f2da7c0
jordanhudgens <jordan@devcamp.com>
2018-12-22 11:57:04 -0600

    D_07_how_and_why_to_use_key_prop_react

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -39,7 +39,14 @@ export default class PortfolioContainer extends Component {
 
   portfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem title={item.name} url={item.url} slug={item.id} />;
+      return (
+        <PortfolioItem
+          key={item.id}
+          title={item.name}
+          url={item.url}
+          slug={item.id}
+        />
+      );
     });
   }
 
