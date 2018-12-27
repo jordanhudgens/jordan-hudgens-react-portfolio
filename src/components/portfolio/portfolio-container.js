0eb48b59d185bc4d6939c4a54666a7e7253c2a79
jordanhudgens <jordan@devcamp.com>
2018-12-27 12:28:45 -0600

    D_09_logging_data_from_an_api

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -39,6 +39,7 @@ export default class PortfolioContainer extends Component {
 
   portfolioItems() {
     return this.state.data.map(item => {
+      console.log("portfolio item", item);
       return (
         <PortfolioItem
           key={item.id}
