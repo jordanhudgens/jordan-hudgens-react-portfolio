b525e7b9175f842b7a8d18ecde026a3cdd24742f
jordanhudgens <jordan@devcamp.com>
2018-12-28 16:26:59 -0600

    D_14_rendering_images_from_api

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -11,6 +11,8 @@ export default function(props) {
   const { id, description, thumb_image_url, logo } = props.item;
   return (
     <div>
+      <img src={thumb_image_url} />
+      <img src={logo} />
       <div>{description}</div>
       <Link to={`/portfolio/${id}`}>Link</Link>
     </div>
