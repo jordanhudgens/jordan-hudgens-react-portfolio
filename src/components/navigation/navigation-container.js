0f2e03fc606b8345ba5fee4af8e27d5e4bef5ed6
jordanhudgens <jordan@devcamp.com>
2018-12-29 14:02:01 -0600

    E_04_building_navigation_link_wrapper_styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -10,21 +10,29 @@ export default class NavigationComponent extends Component {
     return (
       <div className="nav-wrapper">
         <div className="left-side">
-          <NavLink exact to="/" activeClassName="nav-link-active">
-            Home
-          </NavLink>
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+          </div>
 
-          <NavLink to="/about-me" activeClassName="nav-link-active">
-            About
-          </NavLink>
-          <NavLink to="/contact" activeClassName="nav-link-active">
-            Contact
-          </NavLink>
-          <NavLink to="/blog" activeClassName="nav-link-active">
-            Blog
-          </NavLink>
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
 
-          {false ? <button>Add Blog</button> : null}
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact" activeClassName="nav-link-active">
+              Contact
+            </NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">
+              Blog
+            </NavLink>
+          </div>
         </div>
 
         <div className="right-side">JORDAN HUDGENS</div>
