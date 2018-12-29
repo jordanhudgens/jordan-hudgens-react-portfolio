4d29cfe3731edba8f1d96b8b5cc1d778088f14fc
jordanhudgens <jordan@devcamp.com>
2018-12-28 18:00:00 -0600

    E_01_creating_a_main_scss_file_for_imports

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
-  // Data that we'll need:
-  // - background image: thumb_image_url
-  // - logo
-  // - description: description
-  // - id: id
-
   const { id, description, thumb_image_url, logo } = props.item;
   return (
     <div>
