441d35c8cdeff0d532c2c7359a4c72ef72e6e3e1
jordanhudgens <jordan@devcamp.com>
2018-12-31 13:17:44 -0600

    E_10_overlaying_text_images_on_background_img

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
-  const { id, description, thumb_image_url, logo } = props.item;
+  const { id, description, thumb_image_url, logo_url } = props.item;
   return (
     <div className="portfolio-item-wrapper">
       <div
@@ -12,9 +12,13 @@ export default function(props) {
         }}
       />
 
-      <img src={logo} />
-      <div>{description}</div>
-      <Link to={`/portfolio/${id}`}>Link</Link>
+      <div className="img-text-wrapper">
+        <div className="logo-wrapper">
+          <img src={logo_url} />
+        </div>
+
+        <div className="subtitle">{description}</div>
+      </div>
     </div>
   );
 }
