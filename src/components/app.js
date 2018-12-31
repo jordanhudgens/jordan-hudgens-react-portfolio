008d2f2e90cd0d66ce5e040c9eed759bd6e15818
jordanhudgens <jordan@devcamp.com>
2018-12-31 11:46:03 -0600

    E_07_cleaning_up_navigation_bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -16,8 +15,6 @@ export default class App extends Component {
       <div className="app">
         <Router>
           <div>
-            <h1>Jordan Hudgens Portfolio</h1>
-            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationContainer />
 
             <Switch>
