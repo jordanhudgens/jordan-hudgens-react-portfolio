d0e0f42959a4c103104edfc61a8d072da8836a69
jordanhudgens <jordan@devcamp.com>
2019-01-03 11:21:37 -0600

    F_01_overview_of_authentication_behavior

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
@@ -19,6 +20,7 @@ export default class App extends Component {
 
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
