2bbc44f43299fb12f13857c1a0f468582ebb8aba
jordanhudgens <jordan@devcamp.com>
2019-01-07 13:27:17 -0600

    F_15_building_a_route_guard

--- src/components/app.js
+++ src/components/app.js
@@ -65,6 +65,10 @@ export default class App extends Component {
     this.checkLoginStatus();
   }
 
+  authorizedPages() {
+    return [<Route path="/blog" component={Blog} />];
+  }
+
   render() {
     return (
       <div className="container">
@@ -90,7 +94,9 @@ export default class App extends Component {
 
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? (
+                this.authorizedPages()
+              ) : null}
               <Route
                 exact
                 path="/portfolio/:slug"
