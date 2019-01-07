2a6894524c249773eeadff55f7baac91f903f6f5
jordanhudgens <jordan@devcamp.com>
2019-01-07 13:16:37 -0600

    F_14_show_hiding_links__when_logged_in

--- src/components/app.js
+++ src/components/app.js
@@ -70,7 +70,7 @@ export default class App extends Component {
       <div className="container">
         <Router>
           <div>
-            <NavigationContainer />
+            <NavigationContainer loggedInStatus={this.state.loggedInStatus} />
 
             <h2>{this.state.loggedInStatus}</h2>
 
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -2,6 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavigationComponent = props => {
+  const dynamicLink = (route, linkText) => {
+    return (
+      <div className="nav-link-wrapper">
+        <NavLink to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
+      </div>
+    );
+  };
+
   return (
     <div className="nav-wrapper">
       <div className="left-side">
@@ -23,11 +33,9 @@ const NavigationComponent = props => {
           </NavLink>
         </div>
 
-        <div className="nav-link-wrapper">
-          <NavLink to="/blog" activeClassName="nav-link-active">
-            Blog
-          </NavLink>
-        </div>
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          dynamicLink("/blog", "Blog")
+        ) : null}
       </div>
 
       <div className="right-side">JORDAN HUDGENS</div>
