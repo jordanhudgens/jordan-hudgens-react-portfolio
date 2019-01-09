f7ac087daeac8c8f2b8c5007fcd29f2e061b1d06
jordanhudgens <jordan@devcamp.com>
2019-01-09 14:25:18 -0600

    G_03_portfolio_manager_grid_layout_implementation

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,9 +3,14 @@ import React, { Component } from "react";
 export default class PortfolioManager extends Component {
   render() {
     return (
-      <div>
-        <h1>PortfolioManager</h1>
-        <h1>PortfolioManager</h1>
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio form....</h1>
+        </div>
+
+        <div className="right-column">
+          <h1>Portfolio sidebar....</h1>
+        </div>
       </div>
     );
   }
