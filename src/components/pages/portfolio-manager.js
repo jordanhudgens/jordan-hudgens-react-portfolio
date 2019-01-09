53b6db71f3fa2364c2400581493d69e23feb9fd6
jordanhudgens <jordan@devcamp.com>
2019-01-09 14:57:56 -0600

    G_04_populating_portfolio_manager_state_with_api_call_data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
       <div className="portfolio-manager-wrapper">
