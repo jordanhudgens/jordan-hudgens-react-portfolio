d3464508423f8c91ff30e3941a9a14f24d812474
jordanhudgens <jordan@devcamp.com>
2019-01-10 12:03:01 -0600

    G_06_finalizing_portfolio_sidebar_styles

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -3,17 +3,17 @@ import React from "react";
 const PortfolioSidebarList = props => {
   const portfolioList = props.data.map(portfolioItem => {
     return (
-      <div>
-        <div>
+      <div className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
           <img src={portfolioItem.thumb_image_url} />
         </div>
-        <h1>{portfolioItem.name}</h1>
+        <h1 className="title">{portfolioItem.name}</h1>
         <h2>{portfolioItem.id}</h2>
       </div>
     );
   });
 
-  return <div>{portfolioList}</div>;
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 
 export default PortfolioSidebarList;
