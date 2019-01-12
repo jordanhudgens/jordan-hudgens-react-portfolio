29c8055cc77fc99b8363487a913faa5ce6bb8447
jordanhudgens <jordan@devcamp.com>
2019-01-12 14:19:59 -0600

    G_19_uploading_thumbnail_images_to_api

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -24,6 +24,13 @@ export default class PortfolioForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.componentConfig = this.componentConfig.bind(this);
     this.djsConfig = this.djsConfig.bind(this);
+    this.handleThumbDrop = this.handleThumbDrop.bind(this);
+  }
+
+  handleThumbDrop() {
+    return {
+      addedfile: file => this.setState({ thumb_image: file })
+    };
   }
 
   componentConfig() {
@@ -49,6 +56,10 @@ export default class PortfolioForm extends Component {
     formData.append("portfolio_item[url]", this.state.url);
     formData.append("portfolio_item[category]", this.state.category);
     formData.append("portfolio_item[position]", this.state.position);
+
+    if (this.state.thumb_image) {
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+    }
 
     return formData;
   }
@@ -134,6 +145,7 @@ export default class PortfolioForm extends Component {
             <DropzoneComponent
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
+              eventHandlers={this.handleThumbDrop()}
             />
           </div>
 
