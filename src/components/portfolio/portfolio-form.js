0340f89817f1da628c90a22a974182aeffe0a442
jordanhudgens <jordan@devcamp.com>
2019-01-22 14:26:16 -0700

    G_25_updating_dropzone_labels_with_child_components_finalize

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -180,31 +180,39 @@ export default class PortfolioForm extends Component {
           />
         </div>
 
-        <div className="image-uploaders three-column">
+        <div className="image-uploaders">
           <DropzoneComponent
             ref={this.thumbRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleThumbDrop()}
-          />
+          >
+            <div className="dz-message">Thumbnail</div>
+          </DropzoneComponent>
 
           <DropzoneComponent
             ref={this.bannerRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleBannerDrop()}
-          />
+          >
+            <div className="dz-message">Banner</div>
+          </DropzoneComponent>
 
           <DropzoneComponent
             ref={this.logoRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleLogoDrop()}
-          />
+          >
+            <div className="dz-message">Logo</div>
+          </DropzoneComponent>
         </div>
 
         <div>
-          <button type="submit">Save</button>
+          <button className="btn" type="submit">
+            Save
+          </button>
         </div>
       </form>
     );
