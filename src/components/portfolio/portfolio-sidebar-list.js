8423da6079410fde1aada84fefdb01cfaf23825a
jordanhudgens <jordan@devcamp.com>
2019-02-04 11:57:15 -0600

    H_04_how_to_style_font_awesome_icons_react

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -8,11 +8,16 @@ const PortfolioSidebarList = props => {
         <div className="portfolio-thumb-img">
           <img src={portfolioItem.thumb_image_url} />
         </div>
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        <a onClick={() => props.handleDeleteClick(portfolioItem)}>
-          <FontAwesomeIcon icon="trash" />
-        </a>
+
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
+          <a
+            className="delete-icon"
+            onClick={() => props.handleDeleteClick(portfolioItem)}
+          >
+            <FontAwesomeIcon icon="trash" />
+          </a>
+        </div>
       </div>
     );
   });
