b119e1e3b9b2506be1a6d37763a11d3e595de09a
jordanhudgens <jordan@devcamp.com>
2019-02-04 12:46:39 -0600

    H_05_font_awesome_naming_structure_react

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
@@ -64,7 +65,9 @@ const NavigationComponent = props => {
       <div className="right-side">
         JORDAN HUDGENS
         {props.loggedInStatus === "LOGGED_IN" ? (
-          <a onClick={handleSignOut}>Sign Out</a>
+          <a onClick={handleSignOut}>
+            <FontAwesomeIcon icon="sign-out-alt" />
+          </a>
         ) : null}
       </div>
     </div>
