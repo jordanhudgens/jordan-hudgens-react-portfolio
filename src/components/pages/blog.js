fadce5c928c7b68204f653b0ae6352c4c18936c5
jordanhudgens <jordan@devcamp.com>
2019-02-13 13:31:59 -0600

    I_04_rendering_blog_records_to_screen

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -33,15 +33,11 @@ class Blog extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h2>Blog</h2>
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <h1>{blogItem.title}</h1>;
+    });
 
-        <div>
-          <Link to="/about-me">Read more about myself</Link>
-        </div>
-      </div>
-    );
+    return <div>{blogRecords}</div>;
   }
 }
 
