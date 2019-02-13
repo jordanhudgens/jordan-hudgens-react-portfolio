b4f79f747f4d786ebc27d1df0014004574520153
jordanhudgens <jordan@devcamp.com>
2019-02-13 12:49:36 -0600

    I_02_converting_functional_blog_component_into_class_component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super();
+  }
 
+  render() {
+    return (
       <div>
-        <Link to="/about-me">Read more about myself</Link>
+        <h2>Blog</h2>
+
+        <div>
+          <Link to="/about-me">Read more about myself</Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog;
