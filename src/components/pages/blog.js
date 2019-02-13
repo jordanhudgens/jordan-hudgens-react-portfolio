6ffbc58c252d44bdeb3d8956081d711312e09ee5
jordanhudgens <jordan@devcamp.com>
2019-02-13 13:11:08 -0600

    I_03_calling_blog_api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+      blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
   }
 
   render() {
