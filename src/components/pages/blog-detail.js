17328ab44aefd3cb61e56effdad707b9ba1e1aa2
jordanhudgens <jordan@devcamp.com>
2019-02-19 13:14:15 -0800

    I_09_applying_styles_to_blog_detail_component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -40,10 +40,16 @@ export default class BlogDetail extends Component {
     } = this.state.blogItem;
 
     return (
-      <div>
-        <h1>{title}</h1>
-        <img src={featured_image_url} />
-        <div>{content}</div>
+      <div className="blog-container">
+        <div className="content-container">
+          <h1>{title}</h1>
+
+          <div className="featured-image-wrapper">
+            <img src={featured_image_url} />
+          </div>
+
+          <div className="content">{content}</div>
+        </div>
       </div>
     );
   }
