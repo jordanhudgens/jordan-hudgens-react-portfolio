20ab98213f24c72d721a1b9b3af32f065a58b8b2
jordanhudgens <jordan@devcamp.com>
2019-02-19 12:31:56 -0800

    I_07_calling_single_blog_item_from_API

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
+
+  getBlogItem() {
+    axios
+      .get(
+        `https://jordan.devcamp.space/portfolio/portfolio_blogs/${this.state
+          .currentId}`
+      )
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
   render() {
     return (
       <div>
