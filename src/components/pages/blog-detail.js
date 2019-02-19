656685f7852687eaa5e2245398ac0e0a3fccaeb0
jordanhudgens <jordan@devcamp.com>
2019-02-19 12:51:29 -0800

    I_08_rendering_blog_details_to_screen

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -18,7 +18,9 @@ export default class BlogDetail extends Component {
           .currentId}`
       )
       .then(response => {
-        console.log("response", response);
+        this.setState({
+          blogItem: response.data.portfolio_blog
+        });
       })
       .catch(error => {
         console.log("getBlogItem error", error);
@@ -30,9 +32,18 @@ export default class BlogDetail extends Component {
   }
 
   render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
     return (
       <div>
-        <h1>Blog detail</h1>
+        <h1>{title}</h1>
+        <img src={featured_image_url} />
+        <div>{content}</div>
       </div>
     );
   }
