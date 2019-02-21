d0596da8a5026df5e752ae9e3b109f4cab6499b2
jordanhudgens <jordan@devcamp.com>
2019-02-20 16:51:34 -0800

    I_10_applying_styles_to_blog_index_component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -38,7 +38,11 @@ class Blog extends Component {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
 
-    return <div>{blogRecords}</div>;
+    return (
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+      </div>
+    );
   }
 }
 
