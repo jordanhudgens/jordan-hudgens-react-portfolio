22939cb7511682810b8f294603312bbfdfafe5d2
jordanhudgens <jordan@devcamp.com>
2019-02-20 17:44:49 -0800

    I_12_deep_dive_innerheight_scrolltop_offsetheight

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -17,7 +17,12 @@ class Blog extends Component {
 
   activateInfiniteScroll() {
     window.onscroll = () => {
-      console.log("onscroll");
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
     };
   }
 
