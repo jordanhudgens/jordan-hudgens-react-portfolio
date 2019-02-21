ff6dabd6433b57c8e51892c48998ce7a81613210
jordanhudgens <jordan@devcamp.com>
2019-02-21 11:24:01 -0800

    I_13_getting_count_of_total_records_on_server

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,7 +8,9 @@ class Blog extends Component {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
@@ -27,13 +29,18 @@ class Blog extends Component {
   }
 
   getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+
     axios
       .get("https://jordan.devcamp.space/portfolio/portfolio_blogs", {
         withCredentials: true
       })
       .then(response => {
         this.setState({
-          blogItems: response.data.portfolio_blogs
+          blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records
         });
       })
       .catch(error => {
