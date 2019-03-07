fa3447bf75709003ff2bd7f41dfea372baf66660
jordanhudgens <jordan@devcamp.com>
2019-03-07 11:15:40 -0600

    I_39_removing_html_tags_and_building_summaries

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import striptags from "striptags";
+import Truncate from "react-truncate";
 
 const BlogItem = props => {
   const {
@@ -15,7 +17,18 @@ const BlogItem = props => {
       <Link to={`/b/${id}`}>
         <h1>{title}</h1>
       </Link>
-      <div>{content}</div>
+      <div>
+        <Truncate
+          lines={5}
+          ellipsis={
+            <span>
+              ...<Link to={`/b/${id}`}>Read more</Link>
+            </span>
+          }
+        >
+          {striptags(content)}
+        </Truncate>
+      </div>
     </div>
   );
 };
