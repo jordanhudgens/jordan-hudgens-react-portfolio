0353d96a70d825e9f482c42c28a39578db875750
jordanhudgens <jordan@devcamp.com>
2019-03-12 14:55:38 -0500

    I_51_communicating_with_blog_update_api_endpoint

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -13,7 +13,9 @@ export default class BlogForm extends Component {
       title: "",
       blog_status: "",
       content: "",
-      featured_image: ""
+      featured_image: "",
+      apiUrl: "https://jordan.devcamp.space/portfolio/portfolio_blogs",
+      apiAction: "post"
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -50,7 +52,12 @@ export default class BlogForm extends Component {
       this.setState({
         id: this.props.blog.id,
         title: this.props.blog.title,
-        status: this.props.blog.status
+        blog_status: this.props.blog.blog_status,
+        content: this.props.blog.content,
+        apiUrl: `https://jordan.devcamp.space/portfolio/portfolio_blogs/${
+          this.props.blog.id
+        }`,
+        apiAction: "patch"
       });
     }
   }
@@ -98,12 +105,12 @@ export default class BlogForm extends Component {
   }
 
   handleSubmit(event) {
-    axios
-      .post(
-        "https://jordan.devcamp.space/portfolio/portfolio_blogs",
-        this.buildForm(),
-        { withCredentials: true }
-      )
+    axios({
+      method: this.state.apiAction,
+      url: this.state.apiUrl,
+      data: this.buildForm(),
+      withCredentials: true
+    })
       .then(response => {
         if (this.state.featured_image) {
           this.featuredImageRef.current.dropzone.removeAllFiles();
@@ -116,9 +123,14 @@ export default class BlogForm extends Component {
           featured_image: ""
         });
 
-        this.props.handleSuccessfullFormSubmission(
-          response.data.portfolio_blog
-        );
+        if (this.props.editMode) {
+          // Update blog detail
+          this.props.handleUpdateFormSubmission(response.data.portfolio_blog);
+        } else {
+          this.props.handleSuccessfullFormSubmission(
+            response.data.portfolio_blog
+          );
+        }
       })
       .catch(error => {
         console.log("handleSubmit for blog error", error);
--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -17,6 +17,16 @@ export default class BlogDetail extends Component {
 
     this.handleEditClick = this.handleEditClick.bind(this);
     this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+    this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+      this
+    );
+  }
+
+  handleUpdateFormSubmission(blog) {
+    this.setState({
+      blogItem: blog,
+      editMode: false
+    });
   }
 
   handleFeaturedImageDelete() {
@@ -66,6 +76,7 @@ export default class BlogDetail extends Component {
         return (
           <BlogForm
             handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+            handleUpdateFormSubmission={this.handleUpdateFormSubmission}
             editMode={this.state.editMode}
             blog={this.state.blogItem}
           />
