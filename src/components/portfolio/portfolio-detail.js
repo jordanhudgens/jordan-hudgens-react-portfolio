7d0160645402913861a4a07107b8bd00fcc2e52d
jordanhudgens <jordan@devcamp.com>
2019-03-14 15:31:06 -0500

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function(props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  );
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://jordan.devcamp.space/portfolio/portfolio_items/${
+          this.props.match.params.slug
+        }`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("res", response);
+      })
+      .catch(error => {
+        console.log("getportfolioitem error", error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
+    );
+  }
 }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -21,28 +21,30 @@ export default class PortfolioItem extends Component {
   render() {
     const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
-      <div
-        className="portfolio-item-wrapper"
-        onMouseEnter={() => this.handleMouseEnter()}
-        onMouseLeave={() => this.handleMouseLeave()}
-      >
+      <Link to={`/portfolio/${id}`}>
         <div
-          className={
-            "portfolio-img-background " + this.state.portfolioItemClass
-          }
-          style={{
-            backgroundImage: "url(" + thumb_image_url + ")"
-          }}
-        />
+          className="portfolio-item-wrapper"
+          onMouseEnter={() => this.handleMouseEnter()}
+          onMouseLeave={() => this.handleMouseLeave()}
+        >
+          <div
+            className={
+              "portfolio-img-background " + this.state.portfolioItemClass
+            }
+            style={{
+              backgroundImage: "url(" + thumb_image_url + ")"
+            }}
+          />
 
-        <div className="img-text-wrapper">
-          <div className="logo-wrapper">
-            <img src={logo_url} />
+          <div className="img-text-wrapper">
+            <div className="logo-wrapper">
+              <img src={logo_url} />
+            </div>
+
+            <div className="subtitle">{description}</div>
           </div>
-
-          <div className="subtitle">{description}</div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
