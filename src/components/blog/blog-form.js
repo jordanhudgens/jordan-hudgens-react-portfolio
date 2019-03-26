f7e9d24ca7c79ae8062baad460491b0d1dc954cf
jordanhudgens <jordan@devcamp.com>
2019-03-26 11:10:28 -0400

    Testing fix

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import axios from "axios";
 import DropzoneComponent from "react-dropzone-component";
 
+import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
+import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
+
 import RichTextEditor from "../forms/rich-text-editor";
 
 export default class BlogForm extends Component {
